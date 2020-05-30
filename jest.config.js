6a8fe3a1198de87c4d304ec88d7a1dcab3d3f6f1
WagnerAlvesMelo <walves13@outlook.com>
2020-05-30 17:56:33 -0300

    feat: signupController

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
