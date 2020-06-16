b7569f6e6119d0f25c0ddd2ba97bd49dd132271e
WagnerAlvesMelo <walves13@outlook.com>
2020-06-15 21:07:52 -0300

    chore: add features to test scripts

--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('./jest.config.js')
-config.testMatch = ['**/*.spect.ts']
+config.testMatch = ['**/*.spec.ts']
 module.exports = config
